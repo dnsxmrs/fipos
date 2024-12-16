870015b11050c31c360a5fb58931f2bb65993d61
shnlmra <141259116+shnlmra@users.noreply.github.com>
2024-12-16 21:16:31 +0800

    v2 UI

--- resources/views/components/sidebar/admin-sidebar.blade.php
+++ resources/views/components/sidebar/admin-sidebar.blade.php
@@ -1,5 +1,5 @@
 <!-- Sidebar -->
-<div class=" h-screen bg-white border-r flex flex-col items-center py-6">
+<div class="flex flex-col items-center h-screen py-6 bg-white border-r">
 
     <!-- Caffeinated Logo -->
     <div class="mb-1">
@@ -8,28 +8,28 @@
 
     {{-- Dashboard --}}
     <a href="{{ route('admin.dashboard') }}" class="flex flex-col items-center">
-        <div class="w-10 h-10 rounded-lg mb-2"></div>
+        <div class="w-10 h-10 mb-2 rounded-lg"></div>
         <img src="{{ asset('Assets/Graph.png') }}" alt="Reports Icon" class="w-6 h-6 mb-2">
         <span class="text-xs">Dashboard</span>
     </a>
 
     <!-- Menu Labels -->
     <a href="{{ route('admin.menu.categories') }}" class="flex flex-col items-center">
-        <div class="w-10 h-10 rounded-lg mb-2"></div>
+        <div class="w-10 h-10 mb-2 rounded-lg"></div>
         <img src="{{ asset('Assets/food menu icon 1.png') }}" alt="Menu Icon" class="w-6 h-6 mb-1">
         <span class="text-xs">Menu</span>
     </a>
 
     <!--ReportModule-->
     <a href="{{ route('admin.reports') }}" class="flex flex-col items-center">
-        <div class="w-10 h-10 rounded-lg mb-2"></div>
+        <div class="w-10 h-10 mb-2 rounded-lg"></div>
         <img src="{{ asset('Assets/Graph.png') }}" alt="Reports Icon" class="w-6 h-6 mb-2">
         <span class="text-xs">Reports</span>
     </a>
 
     <!--OnlineOrders Module-->
     <a href="{{ route('admin.staff-management') }}" class="flex flex-col items-center">
-        <div class="w-10 h-10 rounded-lg mb-2"></div>
+        <div class="w-10 h-10 mb-2 rounded-lg"></div>
         <img src="path-to-online-icon" alt="Online Orders Icon" class="w-6 h-6 mb-2">
         <span class="text-xs">Staff</span>
         <span class="text-xs">Management</span>
@@ -37,7 +37,7 @@
 
     <!--Order Tracking Module-->
     <a href="{{ route('admin.order-tracking') }}" class="flex flex-col items-center">
-        <div class="w-10 h-10 rounded-lg mb-2"></div>
+        <div class="w-10 h-10 mb-2 rounded-lg"></div>
         <img src="{{ asset('Assets/order report icon 1.png') }}" alt="Order Tracking Icon" class="w-6 h-6 mb-2">
         <div class="text-xs">Order</div>
         <div class="text-xs">Tracking</div>
@@ -45,15 +45,16 @@
 
     <!--Settings-->
     <a href="{{ route('admin.settings') }}" class="flex flex-col items-center">
-        <div class="w-10 h-10 rounded-lg mb-2"></div>
+        <div class="w-10 h-10 mb-2 rounded-lg"></div>
         <img src="{{ asset('Assets/Setting.png') }}" alt="Settings Icon" class="w-6 h-6 mb-2">
         <span class="text-xs">Settings</span>
     </a>
 
-    <div class="mt-20 bg-orange-200 h-10">
-        {{-- sample button for logout --}}
-        <a href="{{ route('logout.confirm') }}" class="big-button">Logout</a>
-    </div>
+    <a href="{{ route('logout.confirm') }}" class="flex flex-col items-center">
+        <div class="w-10 h-10 mb-2 rounded-lg"></div>
+        <img src="{{ asset('Assets/log-out.png') }}" alt="log-out.png" class="w-6 h-6 mb-2">
+        <span class="text-xs">Log out</span>
+    </a>
 </div>
 
 <script>
