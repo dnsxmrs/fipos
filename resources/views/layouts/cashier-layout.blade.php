f41f143f0f372106fa6f16c66f3fb6e9725bd8f8
codebeen <darbenvlamonte@iskolarngbayan.pup.edu.ph>
2025-01-04 19:25:48 +0800

    add tailwind cdn in layouts

--- resources/views/layouts/cashier-layout.blade.php
+++ resources/views/layouts/cashier-layout.blade.php
@@ -14,6 +14,7 @@
     <script defer src="{{ asset('js/cashier/cashier-order.js') }}"></script>
     <script defer src="{{ asset('js/cashier/orders.js') }}"></script>
     <script defer src="{{ asset('js/cashier/cashier-header.js') }}"></script>
+    <script src="https://cdn.tailwindcss.com"></script>
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
