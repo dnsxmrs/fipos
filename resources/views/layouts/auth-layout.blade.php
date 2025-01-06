ecb7258477675bf28bb21608f9e6a8421739e639
shnlmra <141259116+shnlmra@users.noreply.github.com>
2025-01-06 18:32:26 +0800

    fixed layout script

--- resources/views/layouts/auth-layout.blade.php
+++ resources/views/layouts/auth-layout.blade.php
@@ -8,7 +8,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap" rel="stylesheet">
     {{-- <script defer src="{{ asset('js/login.js') }}"></script> --}}
 
-    <script defer src="/js/login.js"></script>
+    <script src="https://fipos-production.up.railway.app/js/login.js"> </script>
 
     <script src="https://cdn.tailwindcss.com"></script>
 
