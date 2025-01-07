17028a4bc0dbd35594d2ea58d671dcf8076d9986
codebeen <darbenvlamonte@iskolarngbayan.pup.edu.ph>
2025-01-07 12:52:16 +0800

    add cdn

--- resources/views/layouts/admin-layout.blade.php
+++ resources/views/layouts/admin-layout.blade.php
@@ -27,7 +27,7 @@
     <script defer src="https://fipos-production.up.railway.app/Assets/js/inventory/inventory-categories.js"> </script>
 
 
-    {{-- <script src="https://cdn.tailwindcss.com"></script> --}}
+    <script src="https://cdn.tailwindcss.com"></script>
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
